356841c13cb9b1d7045f95a6cfd4617694139566
unknown <info.dev.maple@gmail.com>
2018-12-14 00:51:45 +0800

    test

--- models/cookies.js
+++ models/cookies.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 // define the schema for our user model
 var cookiesSchema = mongoose.Schema({
-    cookies: String,
+    cookies: {},
     sessionID: String
 }, { timestamps: { createdAt: 'created_at' } });
 
