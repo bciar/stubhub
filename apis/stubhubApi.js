331f40f0f53aad86caac14e70ad5c9d0a63c302e
unknown <info.dev.maple@gmail.com>
2018-12-14 22:34:04 +0800

    80

--- apis/stubhubApi.js
+++ apis/stubhubApi.js
@@ -89,9 +89,8 @@ class stubhubApi {
         return new Promise((resolve, reject) => {
             request(options, (err, response, body) => {
                 if (err) reject(err);
-                console.log(body);
-                if (body.indexOf('<HTML>') > 0) reject('err');
-                resolve(JSON.parse(body));
+                if (body.indexOf('permission') > 0) reject('err');
+                else { resolve(JSON.parse(body)); }
             });
         })
     }
